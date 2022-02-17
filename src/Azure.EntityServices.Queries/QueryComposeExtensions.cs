be4f262b1fa463f0cdefa3391f1cb7733c3c6684
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2022-02-17 15:48:23 +0100

    chore

--- src/Azure.EntityServices.Queries/QueryComposeExtensions.cs
+++ src/Azure.EntityServices.Queries/QueryComposeExtensions.cs
@@ -7,21 +7,29 @@ namespace Azure.EntityServices.Queries {
     /// </summary>
 public static class IQueryComposeExtensions
     {
-        public static IQueryFilter<T> WherePartitionKey<T>(this IQueryCompose<T> query) => query.AddQuery("PartitionKey");
+        public static IQueryFilter<T> WherePartitionKey<T>(this IQueryCompose<T> query) 
+            => query.AddQuery("PartitionKey");
 
-        public static IQueryFilter<T> WhereRowKey<T>(this IQueryCompose<T> query) => query.AddQuery("RowKey");
+        public static IQueryFilter<T> WhereRowKey<T>(this IQueryCompose<T> query) 
+            => query.AddQuery("RowKey");
 
-        public static IQueryFilter<T> AndRowKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.And), "RowKey");
+        public static IQueryFilter<T> AndRowKey<T>(this IFilterOperator<T> query) 
+            => query.AddOperator(nameof(IQueryInstructions.And), "RowKey");
 
-        public static IQueryFilter<T> NotRowKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.Not), "RowKey");
+        public static IQueryFilter<T> NotRowKey<T>(this IFilterOperator<T> query) 
+            => query.AddOperator(nameof(IQueryInstructions.Not), "RowKey");
 
-        public static IQueryFilter<T> OrRowKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.Or), "RowKey");
+        public static IQueryFilter<T> OrRowKey<T>(this IFilterOperator<T> query)
+            => query.AddOperator(nameof(IQueryInstructions.Or), "RowKey");
 
-        public static IQueryFilter<T> AndPartitionKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.And), "PartitionKey");
+        public static IQueryFilter<T> AndPartitionKey<T>(this IFilterOperator<T> query) 
+            => query.AddOperator(nameof(IQueryInstructions.And), "PartitionKey");
 
-        public static IQueryFilter<T> NotPartitionKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.Not), "PartitionKey");
+        public static IQueryFilter<T> NotPartitionKey<T>(this IFilterOperator<T> query) 
+            => query.AddOperator(nameof(IQueryInstructions.Not), "PartitionKey");
 
-        public static IQueryFilter<T> OrPartitionKey<T>(this IFilterOperator<T> query) => query.AddOperator(nameof(IQueryInstructions.Or), "PartitionKey");
+        public static IQueryFilter<T> OrPartitionKey<T>(this IFilterOperator<T> query) 
+            => query.AddOperator(nameof(IQueryInstructions.Or), "PartitionKey");
     }
 
 }
