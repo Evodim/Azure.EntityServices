72a485713500b316c7356b5903e5e06366ac702d
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2022-05-21 10:03:49 +0200

    update datetime string helper

--- src/Azure.EntityServices.Tables/Extensions/ObjectExtensions.cs
+++ src/Azure.EntityServices.Tables/Extensions/ObjectExtensions.cs
@@ -10,8 +10,8 @@ namespace Azure.EntityServices.Tables.Extensions
         {
             return value switch
             {
-                DateTime v => (v == default(DateTime)) ? new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToString("o") : v.ToString("o"),
-                DateTimeOffset v => (v == default) ? new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc)).ToString("o"): v.ToString("o", CultureInfo.InvariantCulture),
+                DateTime v => (v == default(DateTime)) ? new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToString("o") : v.ToString("o", CultureInfo.InvariantCulture),
+                DateTimeOffset v => (v == default) ? new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, DateTimeKind.Utc)).ToString("o", CultureInfo.InvariantCulture) : v.ToString("o", CultureInfo.InvariantCulture),
                 _ => Convert.ToString(value, CultureInfo.InvariantCulture)
             };
         }
