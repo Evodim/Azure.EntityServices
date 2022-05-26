0fc4cfb908b39c80bfcb8cf78b7f918502e97bc8
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2022-05-26 02:10:36 +0200

    Query introduce dynamic query as extensions

---
+++ src/Azure.EntityServices.Queries/HelperExtensions/DynamicQueryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Azure.EntityServices.Queries
+{
+    public static class DynamicQueryExtensions
+    {
+        public static IFilterOperator<T> WithEach<T, U>(this IFilterOperator<T> query, IEnumerable<U> list, Func<U, IFilterOperator<T>, IFilterOperator<T>> action)
+        {
+            var dynamicQuery = query;
+            foreach (var item in list)
+            {
+                dynamicQuery = action(item, dynamicQuery);
+            }
+            return dynamicQuery;
+        }
+    }
+}
