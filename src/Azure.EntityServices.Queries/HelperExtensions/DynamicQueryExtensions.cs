ad5abaa2061f60e5d1b7524b912f2c5e8f9e0174
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2022-05-26 02:10:59 +0200

    chore

--- src/Azure.EntityServices.Queries/HelperExtensions/DynamicQueryExtensions.cs
+++ src/Azure.EntityServices.Queries/HelperExtensions/DynamicQueryExtensions.cs
@@ -5,6 +5,15 @@ namespace Azure.EntityServices.Queries
 {
     public static class DynamicQueryExtensions
     {
+        /// <summary>
+        /// Build query filters by iterate on each given collection item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U">Generic collection</typeparam>
+        /// <param name="query"></param>
+        /// <param name="list"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
         public static IFilterOperator<T> WithEach<T, U>(this IFilterOperator<T> query, IEnumerable<U> list, Func<U, IFilterOperator<T>, IFilterOperator<T>> action)
         {
             var dynamicQuery = query;
