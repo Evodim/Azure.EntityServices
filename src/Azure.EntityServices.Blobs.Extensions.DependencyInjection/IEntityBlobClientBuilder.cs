540e76d086e7a327cff2bb888fe3b82ab38e56ba
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2023-02-05 01:26:48 +0100

    added new prj for blobs DI

---
+++ src/Azure.EntityServices.Blobs.Extensions.DependencyInjection/IEntityBlobClientBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Azure.EntityServices.Blobs;
+using System;
+
+namespace Azure.EntityServices.Tables.Extensions.DependencyInjection
+{
+    public interface IEntityBlobClientBuilder<TEntity>
+    {
+        IEntityBlobClientBuilder<TEntity> ConfigureEntity(Action<EntityBlobClientConfig<TEntity>> configureEntity);
+
+        IEntityBlobClientBuilder<TEntity> ConfigureEntity(Action<IServiceProvider, EntityBlobClientConfig<TEntity>> configureEntity);
+
+        IEntityBlobClientBuilder<TEntity> ConfigureOptions(Action<EntityBlobClientOptions> configureOptions);
+
+        (EntityBlobClientOptions, EntityBlobClientConfig<TEntity>) Build(IServiceProvider serviceProvider);
+    }
+}
