ec3e3eb7122c559d332efff1902d05682ac772bb
devteam <devteam@evodim.com>
2024-04-17 23:54:37 +0200

    update target frameworks

--- samples/BlobClient.Sample/Program.cs
+++ samples/BlobClient.Sample/Program.cs
@@ -1,13 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace BlobClient.BasicSample
-{
-    public static class Program
-    {
-        private static async Task Main()
-        {
-            await BlobSample.Run();
-            System.Console.ReadLine();
-        }
-    }
-}
+﻿using BlobClient.BasicSample;
+ 
+await BlobSample.RunAsync();
+System.Console.ReadLine();
